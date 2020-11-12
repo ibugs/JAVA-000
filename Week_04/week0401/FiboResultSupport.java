f2c0591c8e6acfee51cec58118b703dac0ac514e
wanghao <wanghao293@126.com>
2020-11-12 08:02:14 +0800

    第7节，第9种方式

--- Week_04/week0401/FiboResultSupport.java
+++ Week_04/week0401/FiboResultSupport.java
@@ -2,4 +2,6 @@ package week0401;
 
 class FiboResultSupport {
     Integer result = 0;
+    boolean created = false;
+    Integer count = 0;
 }
--- Week_04/week0401/Homework0309.java
+++ Week_04/week0401/Homework0309.java
@@ -9,8 +9,83 @@ package week0401;
  */
 public class Homework0309 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        long start = System.currentTimeMillis();
+        FiboResultSupport fiboResultSupport = new FiboResultSupport();
 
+        Product p = new Product(fiboResultSupport);
+        Customer c = new Customer(fiboResultSupport);
+
+        Thread t1 = new Thread(p);
+        Thread t2 = new Thread(c);
+        t1.start();t2.start();
+        // 1. 加上这两步就相当于从主线程获得结果
+        // 2. 去掉这两步就相当于从子线程获得结果
+        t1.join();t2.join();
+        long end = System.currentTimeMillis();
+
+        System.out.println("获得异步结果"+fiboResultSupport.result);
+        System.out.println("耗时"+(end - start)+"ms");
     }
 }
 
+class Product implements Runnable{
+    private FiboResultSupport resultSupport;
+
+    public Product (FiboResultSupport resultSupport){
+       this.resultSupport = resultSupport;
+    }
+
+    @Override
+    public void run() {
+        synchronized (resultSupport) {
+            try {
+                while (true) {
+                    if (resultSupport.count == 0) {
+                        resultSupport.created = true;
+                        resultSupport.count = 1;
+                        System.out.println("生产者生产完毕");
+                        resultSupport.notifyAll();
+                        break;
+                    } else {
+                        System.out.println("生产者再等待结果");
+                        resultSupport.wait();
+                    }
+                }
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class Customer implements Runnable {
+    private FiboResultSupport fiboResultSupport;
+
+    public Customer(FiboResultSupport fiboResultSupport){
+       this.fiboResultSupport = fiboResultSupport;
+    }
+
+    @Override
+    public void run() {
+       synchronized (fiboResultSupport) {
+           while (true) {
+               try {
+                   if (fiboResultSupport.count >= 1) {
+                       FiboCall fiboCall = new FiboCall();
+                       fiboResultSupport.result = fiboCall.sum();
+                       //fiboResultSupport.count = 0;
+                       System.out.println("消费者已经计算完毕，结果为"+fiboResultSupport.result);
+                       fiboResultSupport.notifyAll();
+                       break;
+                   } else {
+                       System.out.println("消费者在等待");
+                       fiboResultSupport.wait();
+                   }
+               }catch (InterruptedException e){
+                   e.printStackTrace();
+               }
+           }
+       }
+    }
+}
