cf70447eae4f8e5424e1e8bf7bd056a371535ba6
wanghao <wanghao@ktjr.com>
2020-11-16 19:03:15 +0800

    第10讲作业，使用Statement addBatch来进行批量操作

--- Week_05/src/main/java/com/jdbc/demo/Test.java
+++ Week_05/src/main/java/com/jdbc/demo/Test.java
@@ -1,9 +1,6 @@
 package com.jdbc.demo;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * @author wanghao
@@ -17,7 +14,8 @@ public class Test {
         // t.query();
         // t.delete();
         // t.query();
-        t.update();
+        // t.update();
+        t.insertBatch();
     }
 
     /**
@@ -29,18 +27,44 @@ public class Test {
 
         try {
             connection = JdbcUtils.getConnection();
+            // 设置关闭自动提交
+            connection.setAutoCommit(false);
             String sql = "update work_date set work_date_flag = ?, trade_date_flag = ? where nature_date = ? ";
 
+            /***
+             * 原始数据
+             * mysql> select * from work_date where nature_date = 20191222;
+             * +-------------+----------------+-----------------+
+             * | nature_date | work_date_flag | trade_date_flag |
+             * +-------------+----------------+-----------------+
+             * |    20191222 | 0              | 0               |
+             * +-------------+----------------+-----------------+
+             * 1 row in set (0.02 sec)
+             */
             preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setInt(1, 0);
-            preparedStatement.setInt(2, 0);
+            preparedStatement.setInt(1, 1);
+            preparedStatement.setInt(2, 2);
             preparedStatement.setInt(3, 20191222);
-
             int resultnum = preparedStatement.executeUpdate();
+
+            /*** 一个可以引起异常的例子，出现异常之后回滚 ***/
+            // sql = "updated work_date set wok_date = ?";
+            // preparedStatement = connection.prepareStatement(sql);
+            // preparedStatement.setInt(1, 111);
+
+            // resultnum = preparedStatement.executeUpdate();
+
             System.out.println("更新影响的条数为=>" + resultnum);
+            connection.commit();
         } catch (SQLException e) {
+            // 出现异常就回滚
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
             e.printStackTrace();
-        }finally {
+        } finally {
             JdbcUtils.releaseDB(connection, preparedStatement, null);
         }
     }
@@ -66,6 +90,49 @@ public class Test {
         } finally {
             JdbcUtils.releaseDB(connection, preparedStatement, null);
         }
+
+    }
+
+
+    /***
+     * 批量插入，使用Statement. addBatch
+     */
+    public void insertBatch() {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = JdbcUtils.getConnection();
+            Statement statement = connection.createStatement();
+            connection.setAutoCommit(false);
+            String sql = "INSERT INTO work_date(nature_date, work_date_flag, trade_date_flag)" +
+                    "VALUES(20201117,1,1)";
+
+            statement.addBatch(sql);
+
+            sql = "INSERT INTO work_date(nature_date, work_date_flag, trade_date_flag)" +
+                    "VALUES(20201118,2,2)";
+
+            statement.addBatch(sql);
+
+            sql = "UPDATE work_date set work_date_flag = 9, trade_date_flag = 9 where nature_date = 20201116";
+            statement.addBatch(sql);
+
+            int[] resultCount = statement.executeBatch();
+            connection.commit();
+
+            System.out.println(resultCount[0] + "->" + resultCount[1] + "->" + resultCount[2]);
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            e.printStackTrace();
+        }finally {
+            JdbcUtils.releaseDB(connection, null, null);
+        }
+
 
     }
 
